6b233877a6fd6923f53eed577d21500718d04fbc
Thomas Nill <t.nill@t-online.de>
2017-08-14 22:28:54 +0200

    Wegen Sonar und Code Coverage

---
+++ XMLReader/src/main/java/janus/reader/ReaderRuntimeException.java
@@ -0,0 +1,30 @@
+package janus.reader;
+
+public class ReaderRuntimeException extends RuntimeException {
+
+    public ReaderRuntimeException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ReaderRuntimeException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ReaderRuntimeException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ReaderRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ReaderRuntimeException(String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+}
